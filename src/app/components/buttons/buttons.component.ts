1583a1782440b4d4455502b1aee0f15e29e31320
BekkaliyevAssan <bekkaliyevassan@gmail.com>
2020-09-22 20:45:42 +0600

    hllo|

--- src/app/components/buttons/buttons.component.ts
+++ src/app/components/buttons/buttons.component.ts
@@ -14,6 +14,7 @@ export class ButtonsComponent implements OnInit {
   public array = []
   public typeOfPoeration: string
   public operations = ['+', '-', '*', '/']
+  public multDelOperations = ['*', '/']
   constructor(
     private calcService: CalculatorUnderhoodService
   ) { }
@@ -22,11 +23,19 @@ export class ButtonsComponent implements OnInit {
   }
 
   onKeyEnter(val) {
-    if (val === '.' && this.enteredValue.includes('.')) {
-      console.log('there is already .')
+    if (val === '.' && this.enteredValue.includes('.') || val === '√' && this.enteredValue[this.enteredValue.length - 1] === '√') {
+      console.log('there is repeated sign')
     }
+    if(val === '%' && this.enteredValue[this.enteredValue.length - 1] === '%' || this.enteredValue[this.enteredValue.length - 1] === '%' && !this.operations.includes(val)) {
+      console.log('value after % not allowed')
+    }
+    // else if(val === '%' && this.enteredValue.charAt(0) === '%' || val === '√' && this.enteredValue[this.enteredValue.length - 1] === '%') {
+    //   console.log('there is smth wrong with %')
+    // }
     else {
-      if (this.operations.includes(val) && val == this.enteredValue[this.enteredValue.length - 1] || val === '.' && val == this.enteredValue[this.enteredValue.length - 1]) {
+      if (this.operations.includes(val) && val == this.enteredValue[this.enteredValue.length - 1] || 
+          val === '.' && val == this.enteredValue[this.enteredValue.length - 1] || 
+          this.operations.includes(val) && this.operations.includes(this.enteredValue[this.enteredValue.length - 1])) {
         console.log('repeated')
       } else {
         if (this.enteredValue === '' && this.operations.includes(val) || val === '0' && this.enteredValue === '') {
@@ -51,6 +60,8 @@ export class ButtonsComponent implements OnInit {
   clear() {
     this.enteredValue = ''
     this.buttonClick.emit(this.enteredValue)
+    this.tempArray = []
+    this.operatorArray = []
   }
 
   backspace() {
@@ -59,21 +70,6 @@ export class ButtonsComponent implements OnInit {
   }
 
   onKeyEnterPercent() {
-    if(this.enteredValue !== '' && this.enteredValue[this.enteredValue.length - 1] !== '%') {
-      this.enteredValue += '%'
-      this.buttonClick.emit(this.enteredValue)
-    }
-    else console.log('operator error')
-  }
-
-  onKeyEnterRoot() {
-    // if(this.enteredValue[this.enteredValue.length - 1] !== '√') {
-    //   this.enteredValue += '√'
-    //   this.buttonClick.emit(this.enteredValue)
-    // }
-    //   else {
-    //   console.log('root square repeat')
-    // }
   }
 
   public tempArray = []
@@ -87,29 +83,52 @@ export class ButtonsComponent implements OnInit {
         this.tempArray.push({ operator: this.enteredValue.charAt(i), value: value[0] })
       }
     }
+    
+    let firstVal = this.tempArray[0].value
+    //check if first value has a root sqrt
+    if(firstVal.includes('√') && firstVal.charAt(0) == '√') {
+      firstVal = Math.sqrt(firstVal.substring(1))
+    }
+    else if(firstVal.includes('√') && firstVal.charAt(0) != '√') {
+      let tempVal = firstVal.split('√')
+      firstVal = tempVal[0] * Math.sqrt(tempVal[1])
+    }
+    else if(firstVal.includes('%')) {
+      firstVal = parseInt(firstVal)/100
+    }
+    //set first value of the entered value
+    let tempRes = firstVal
+    //check if second operation is root or not
 
-    let tempRes
     console.log(this.tempArray)
-    this.tempArray.forEach((item, index) => {
-      //if items value includes percentage and operator before is multiplication -> first make calculation then make a result.
-      if(item.value.includes('%') && item.operator == '*' || item.operator == '/') {
-        let tempPer = parseInt(item.value) / 100
-        item.value = tempPer.toString()
-      }
-      if(item.value.includes('%') && item.operator == '+' || item.operator == '-') {
-        let tempFirstVal = this.tempArray[index - 1].value
-        let tempSecondVal = this.tempArray[index].value
-        let tempResult = parseInt(tempFirstVal) * parseInt(tempSecondVal) / 100
-        console.log(tempResult)
-        //пизец костыль
-        this.tempArray[index].value = 0
-        this.tempArray[index - 1].value = tempResult.toString()
-      }
-      if(index == 0) tempRes = item.value
-      else
-      tempRes += item.operator + item.value
-    })
-    this.result = eval(tempRes)
-    this.buttonClick.emit(this.result.toString())
+  for(let i = 1; i < this.tempArray.length; i++) {
+
+    let currentVal = this.tempArray[i].value
+    let currentOperator = this.tempArray[i].operator
+    //for root sqrt without value before the root
+    if(currentVal.includes('√') && currentVal.charAt(0) == '√') {
+      let tempVal = currentVal.substring(1)
+      this.tempArray[i].value = Math.sqrt(tempVal)
+    }
+    //for root sqrt with value before 
+    else if( currentVal.includes('√') && currentVal.charAt(0) != '√') {
+      let tempVal = currentVal.split('√')
+      this.tempArray[i].value = tempVal[0] * Math.sqrt(tempVal[1])
+    }
+    //does the current operation has a % and it has multi del operations
+    if(currentVal.includes('%') && this.multDelOperations.includes(currentOperator)) {
+      this.tempArray[i].value = parseInt(currentVal) / 100
+      console.log('there is multi || del operation on this value')
+    }
+    //does the current operation has a % and it has'n mulit del operations
+    else if(currentVal.includes('%') && !this.multDelOperations.includes(currentOperator)) {
+      let firstOperand = this.tempArray[i - 1].value
+      let percent = parseInt(firstOperand) / 100 * parseInt(currentVal)
+      this.tempArray[i].value = percent
+    }
+    tempRes += this.tempArray[i].operator + this.tempArray[i].value
+  }
+  //show the final result
+  console.log(eval(tempRes))
   }
 }
